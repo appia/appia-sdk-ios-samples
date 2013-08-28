d6669719a600310aee828936e3551957e000fc5e
Brian Watkins <bwatkins@littlegreensoftware.com>
2013-08-28 15:05:54 -0400

    Initial commit of AppiaPopupSample

---
+++ AppiaPopupSample/AppiaPopupSample/APViewController.h
@@ -0,0 +1,13 @@
+//
+//  APViewController.h
+//  AppiaPopupSample
+//
+//  Created by Brian Watkins on 8/28/13.
+//  Copyright (c) 2013 Appia, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APViewController : UIViewController
+
+@end
---
+++ AppiaPopupSample/AppiaPopupSample/APViewController.m
@@ -0,0 +1,29 @@
+//
+//  APViewController.m
+//  AppiaPopupSample
+//
+//  Created by Brian Watkins on 8/28/13.
+//  Copyright (c) 2013 Appia, Inc. All rights reserved.
+//
+
+#import "APViewController.h"
+
+@interface APViewController ()
+
+@end
+
+@implementation APViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
